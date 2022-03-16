d2c54372ba44427b631e81b026698a47c74c688e
Chintan Sudani <csudani7@gmail.com>
2022-03-16 19:45:27 +0530

    feat: Added Project structure Libs & File structure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  purge: {
+    content: ['./src/**/*.js', './src/**/*.tsx', './components/**/*.tsx'],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
